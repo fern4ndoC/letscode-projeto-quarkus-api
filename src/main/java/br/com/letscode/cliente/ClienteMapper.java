6b39cb4c12029eaa9b0205c50b13792249cb4ed0
Fernando Carlos <fernando.carlos@bb.com.br>
2022-08-15 15:27:51 -0300

    Corrigindo uso de @ApplicationScoped

--- src/main/java/br/com/letscode/cliente/ClienteMapper.java
+++ src/main/java/br/com/letscode/cliente/ClienteMapper.java
@@ -5,6 +5,7 @@ import org.mapstruct.Mapper;
 import javax.enterprise.context.ApplicationScoped;
 
 @Mapper(componentModel = "cdi")
+@ApplicationScoped
 public interface ClienteMapper {
     ClienteDTO toDomain(Cliente cliente);
     Cliente toEntity(ClienteDTO clienteDTO);
--- src/main/java/br/com/letscode/cliente/ClienteResource.java
+++ src/main/java/br/com/letscode/cliente/ClienteResource.java
@@ -13,7 +13,6 @@ import java.util.List;
 @Path("/cliente")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-@ApplicationScoped
 public class ClienteResource {
 
     @Inject
